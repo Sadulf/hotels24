98fff76568e0e8d2022322009da24c770577b9a8
Sadulf <sandulf@mail.ru>
2016-05-10 14:19:44 +0300

    Авторучка из обычной ручки

---
+++ autopen.php
@@ -0,0 +1,50 @@
+<?php
+namespace autopen;
+
+include __DIR__.'/pen.php';
+use pen\Pen;
+
+/**
+ * Для того чтобы начать писать, авторучку необходимо сначала включить.
+ */
+class AutoPen extends Pen
+{
+	private $_opened = false;
+
+	/**
+	 * Открывает (включает) авторучку, после чего ею можно писать. Если чернила закончились - ручка не включается.
+	 * @return bool если удалось включить ручку - возвращает true, иначе false. 
+	 */
+	public function open()
+	{
+		if($this->_opened)
+			return true;
+		if($this->_ink_residue==0)
+			return false;
+		$this->_opened = true;
+		return true;
+	}
+
+	/**
+	 * Закрывает (выключает) авторучку, после чего писать ею нельзя.
+	 * @return bool если удалось выключить ручку - возвращает true, иначе false. 
+	 */
+	public function close()
+	{
+		$this->_opened = false;
+		return true;
+	}
+
+	/**
+	 * написать что-то (уменьшает запас чернила)
+	 * @param  string $text Текст, который необходимо написать
+	 * @return int       число символов, которые были записаны, или false при ошибке
+	 */
+	public function write($text=null)
+	{
+		// проверим, включена ли ручка
+		if(!$this->_opened)
+			return false;
+		return parent::write($text);
+	}
+} 
